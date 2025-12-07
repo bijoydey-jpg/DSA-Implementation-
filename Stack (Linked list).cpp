253d36ff7c9ceb17962228ac04d6dc60981fbb8c
bijoydey-jpg <131778940+bijoydey-jpg@users.noreply.github.com>
2025-12-07 23:55:08 +0600

    Update Stack (Linked list).cpp

--- Stack (Linked list).cpp
+++ Stack (Linked list).cpp
@@ -1,84 +1,60 @@
 #include<iostream>
 using namespace std;
 
-int size = 0 ;
- int MAX = 4 ;
-
-
 struct Node{
-    int info;
+    int data;
     Node *next;
 };
-
-Node *front = nullptr;
-Node *rare = nullptr;
-
-void enque(int d){
-
-    if(size == MAX) {
-        cout << "FULL" << endl;
+Node *top = nullptr;
+void push(int item){
+    Node *New = new Node{item, nullptr};
+    New->next = top;
+    top = New;
+}
+void pop(){
+    if(top == nullptr){
+        cout << "Underflow\n";
         return;
     }
-
-    Node  *newnode = new Node{d,NULL};
-
-    if(front == NULL){
-
-        front = rare = newnode ;
-    }
-    else {
-
-        rare -> next = newnode;
-        rare = newnode;
-    }
-
-    size++;
-
-}
-
-void deque(){
-
-    if(front == NULL){
-
-        cout << "EMPTY" << endl;
-    }
-    
-    Node *temp = front ;
-    front = front -> next ;
-
-    if(front == NULL){
-        rare = NULL;
-    }
+    Node *temp = top;
+    top = top->next;
     delete temp;
-    size--;
-
-
-    
 }
+
 void show(){
-    Node *ptr = front;
+    Node *ptr = top;
     if(ptr == nullptr){
         cout << "Empty List\n";
         return;
     }
     while(ptr != nullptr){
-        cout << ptr->info;
+        cout << ptr->data;
         if(ptr->next != nullptr)
             cout << "->";
         ptr = ptr->next;
     }
     cout << endl;
 }
+
 main()
 {
-    enque(5);
+    pop();
+    push(5);
     show();
-    enque(5);
-    enque(5);
-    enque(5);
-    enque(5);
+    pop();
     show();
-    deque();
+    push(6);
     show();
-
+    push(7);
+    show();
+    push(8);
+    show();
+    pop();
+    show();
+    pop();
+    show();
+    pop();
+    show();
+    pop();
+    show();
 }
